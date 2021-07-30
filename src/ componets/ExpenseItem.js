2aacfa0633b0732b459e651f054449bc5df73a0f
Himanshu <yadavhimanshu51@gmail.com>
2021-07-30 10:55:31 +0530

    some changes

---
+++ src/ componets/ExpenseItem.js
@@ -0,0 +1,10 @@
+import '../css/ExpenseItem.css'
+
+function ExpenseItem(){
+    return (
+        <div className="my-title">
+            Inside Expense Item.
+        </div>
+    );
+}
+export default ExpenseItem;
